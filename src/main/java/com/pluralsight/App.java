71cb1ab36a431c87a7c4b30fd55438a658089152
yuujn <pale.cup5223@fastmail.com>
2026-05-07 14:00:52 -0500

    add Player class; read in players; deal cards

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -1,4 +1,35 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
+    static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.print("Enter the number of players: ");
+        int playerCount = Integer.parseUnsignedInt(scan.nextLine());
+
+        ArrayList<Player> players = new ArrayList<>();
+        for (int i = 0; i < playerCount; i++) {
+            System.out.printf("Enter Player %d's name: ", i + 1);
+            String name = scan.nextLine();
+            players.add(new Player(name, new Hand()));
+        }
+
+        Deck deck = new Deck();
+
+        // Deal.
+        for (Player player : players) {
+            Hand hand = player.getHand();
+            hand.deal(deck.deal());
+            hand.deal(deck.deal());
+        }
+
+        // TODO:
+        // 1. Display hand
+        // 2. Keep track of who has the highest score below or equal to 21
+        for (Player player : players) {
+
+        }
+    }
 }
---
+++ src/main/java/com/pluralsight/Player.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Player {
+    private String name;
+    private Hand hand;
+
+    public Player(String name, Hand hand) {
+        this.name = name;
+        this.hand = hand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Hand getHand() {
+        return hand;
+    }
+}
